e46d9959c74503a28bdc33b91e540f58359816a8
yurtsevenist <yurtsevenist@hotmail.com>
2024-10-14 12:21:05 +0300

    dashboard

---
+++ dashboard/aside.php
@@ -0,0 +1,15 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
+            <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link"> <!--begin::Brand Image--> <img src="tema/dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+            <div class="sidebar-wrapper">
+                <nav class="mt-2"> <!--begin::Sidebar Menu-->
+                    <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item"> <a href="dashboard.php" class="nav-link active"> <i class="nav-icon bi bi-house-door"></i>
+                                        <p>Dashboard</p>
+                                    </a> </li>
+                                    <li class="nav-item"> <a href="php/logout.php" class="nav-link"> <i class="nav-icon bi bi-box-arrow-right"></i>
+                                        <p>Oturumu Kapat</p>
+                                    </a> </li>
+                    </ul> <!--end::Sidebar Menu-->
+                </nav>
+            </div> <!--end::Sidebar Wrapper-->
+        </aside> <!--end::Sidebar--> <!--begin::App Main-->
